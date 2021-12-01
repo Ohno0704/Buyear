9167298e63cdb79cf50445462ec3aad8b8f537b9
aishoku17127 <68172397+aishoku17127@users.noreply.github.com>
2021-12-01 21:52:37 +0900

    商品ウィジェットに値札をつけたよん

--- lib/routes/Home/HomePage.dart
+++ lib/routes/Home/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 import 'package:flutter_application_1/routes/Home/Account/MyPage.dart';
 import 'package:flutter_application_1/routes/Home/ItemPage.dart';
+
 var itemNum = 30;
 class HomePage extends StatelessWidget {
   // HomePage(this.itemAdder);
@@ -29,33 +30,82 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: Container(
-        width: double.infinity,
-        child: GridView.count(
-          crossAxisCount: 3,
-          children: List.generate(itemNum, (index) {
-            return Column(
-              children: <Widget>[
-                GridTile(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ItemPageAndAddFriend(itemNum-index),
-                        ),
-                      );
-                    },
-                    child: Image.asset(
-                      'images/buyear_rogo.jpeg',
-                      fit: BoxFit.cover,
-                    ),
+      width: double.infinity,
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(itemNum, (index) {
+          return Column(
+            children: <Widget>[
+              GridTile(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ItemPageAndAddFriend(index),
+                      ),
+                    );
+                  },
+                  child: Image.asset(
+                    'images/buyear_rogo.jpeg',
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ],
-            );
-          }
-          )
-        ),
-      )
-    );
+              ),
+              Container(
+                // color: Colors.red,
+                child: Text(
+                  "￥$index",
+                  style: TextStyle(
+                      fontSize: 16.0, fontWeight: FontWeight.w400, fontFamily: "Roboto"),
+                ),
+              ),
+            ],
+          );
+        }
+        )
+      ),
+    )
+      // body: GridView.count(
+      //     padding: EdgeInsets.all(4.0),
+      //     crossAxisCount: 2,
+      //     crossAxisSpacing: 10.0, // 縦
+      //     mainAxisSpacing: 10.0, // 横
+      //     childAspectRatio: 0.7, // 高さ
+      //     shrinkWrap: true,
+      //     children: List.generate(50, (index) {
+
+      //       // var rng = new Random();
+      //       // var imgNumber = rng.nextInt(3);
+      //       var assetsImage = "images/buyear_rogo2.png";
+
+      //       return Container(
+      //         alignment: Alignment.center,
+      //         decoration: BoxDecoration(
+      //           color: Colors.white,
+      //           boxShadow: [
+      //             new BoxShadow(
+      //               color: Colors.grey,
+      //               offset: new Offset(5.0, 2.0),
+      //               blurRadius: 10.0,
+      //             )
+      //           ],
+      //         ),
+      //         child:Column(
+      //           children: <Widget>[
+      //           //   width: 154,
+      //           // height: 230,
+      //             Image.asset(assetsImage, fit: BoxFit.fitWidth,),
+      //             Container(
+      //               margin: EdgeInsets.all(32.0),
+      //               child: Text(
+      //                 '￥ $index',
+      //               ),
+      //             ),
+      //           ]
+      //         ),
+      //       );
+      //     }),
+      //   ),
+      );
   }
 }
